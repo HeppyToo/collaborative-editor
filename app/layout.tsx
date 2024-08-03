abf9cf99ddc31aa8c95682f91d94a9e68f0d4830
HeppyTwo <smykdev15@gmail.com>
2024-08-03 15:38:17 +0300

    authentication - clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,10 @@ import {Inter as FontSans} from "next/font/google"
 import './globals.css'
 import {cn} from "@/lib/utils"
 import {Metadata} from "next";
+import {
+    ClerkProvider,
+} from '@clerk/nextjs'
+import {dark} from "@clerk/themes";
 
 export const metadata: Metadata = {
     title: "LiveDocs",
@@ -16,15 +20,20 @@ const fontSans = FontSans({
 
 export default function RootLayout({children}: { children: React.ReactNode }) {
     return (
-        <html lang="en" suppressHydrationWarning>
-        <body
-            className={cn(
-                "min-h-screen font-sans antialiased",
-                fontSans.variable
-            )}
-        >
-        {children}
-        </body>
-        </html>
+        <ClerkProvider appearance={{
+            baseTheme: dark,
+            variables: {colorPrimary: "#3371FF", fontSize: '16px'}
+        }}>
+            <html lang="en" suppressHydrationWarning>
+            <body
+                className={cn(
+                    "min-h-screen font-sans antialiased",
+                    fontSans.variable
+                )}
+            >
+            {children}
+            </body>
+            </html>
+        </ClerkProvider>
     )
 }
