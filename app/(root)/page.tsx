d56b96e982f1de8c1dfe7ec7a22e1367053bba56
HeppyTwo <smykdev15@gmail.com>
2024-08-03 19:47:23 +0300

    Authenticate User with Real-time Features

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,16 @@
 import {Button} from "@/components/ui/button";
+import Header from '@/components/Header';
 
 const Home = () => {
     return (
         <main className="home-container">
-            <Button>Click me</Button>
+            <Button>
+              <Header className="sticky left-0 top-0">
+                <div className="flex items-center gap-2 lg:gap-4">
+                  Notifications
+                </div>
+              </Header>
+            </Button>
         </main>
     );
 }
